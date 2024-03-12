cd43a2bf89a18c48adb6fde92c22505d9b94e834
gcobos3675 <156113044+gcobos3675@users.noreply.github.com>
2024-03-11 23:14:12 -0400

    Add files via upload

---
+++ chapter10/lab1/scripts/script.js
@@ -0,0 +1,16 @@
+/* 
+Name: Giovanni Cobos
+File name: scripts.js
+Date: March 11, 2024
+*/
+
+// Hamburger menu function
+
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}

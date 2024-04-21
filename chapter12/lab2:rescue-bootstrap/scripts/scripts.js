95e992cd1c6908822dddb8f79b45efa46d2b3638
gcobos3675 <156113044+gcobos3675@users.noreply.github.com>
2024-04-21 02:03:52 -0400

    Delete chapter12/lab2:rescue-bootstrap directory

--- chapter12/lab2:rescue-bootstrap/scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-/*
-    Name: Giovanni Cobos
-    Date: April 20, 2024
-*/
-
-//jQuery for hero image to consume the header window space
-    $(document).ready(function () {
-    $('.hero').height($(window).height());
-});

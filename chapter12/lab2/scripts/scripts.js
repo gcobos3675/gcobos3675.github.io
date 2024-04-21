6574a3014c834d25f379899d0c41b8af20b5d95f
gcobos3675 <156113044+gcobos3675@users.noreply.github.com>
2024-04-21 02:06:33 -0400

    updated lab 2 link to work

---
+++ chapter12/lab2/scripts/scripts.js
@@ -0,0 +1,9 @@
+/*
+    Name: Giovanni Cobos
+    Date: April 20, 2024
+*/
+
+//jQuery for hero image to consume the header window space
+    $(document).ready(function () {
+    $('.hero').height($(window).height());
+});
